072a5781c165934ba256393a53f4aa798485737d
Chraeten <chraetenlategan@gmail.com>
2025-12-17 21:11:36 +0200

    Initialize chess board in main_chess.py

---
+++ main_chess.py
@@ -0,0 +1,21 @@
+# The chess board is added into a 2D list
+board = [
+    ["r","n","b","q","k","b","n","r"],
+    ["p","p","p","p","p","p","p","p"],
+    [".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".","."],
+    [".",".",".",".",".",".",".","."],
+    ["P","P","P","P","P","P","P","P"],
+    ["R","N","B","Q","K","B","N","R"]
+]
+
+
+
+
+#funtion is called to display
+from chess_function import DisplayBoard
+DisplayBoard(board)
+
+
+
