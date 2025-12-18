367e6f101671443e7ecd1413a84d1a15448477a3
Chraeten <chraetenlategan@gmail.com>
2025-12-18 21:47:40 +0200

    Update main_chess.py

--- main_chess.py
+++ main_chess.py
@@ -1,21 +1,22 @@
+from chess_function import DisplayBoard,check_available_moves,check_turns
+
 # The chess board is added into a 2D list
 board = [
-    ["r","n","b","q","k","b","n","r"],
-    ["p","p","p","p","p","p","p","p"],
-    [".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".","."],
-    [".",".",".",".",".",".",".","."],
-    ["P","P","P","P","P","P","P","P"],
-    ["R","N","B","Q","K","B","N","R"]
+    ["r","n","b","q","k","b","n","r"], 
+    ["p","p","p","p","p","p","p","p"], 
+    [".",".",".",".",".",".",".","."], 
+    [".",".",".",".",".",".",".","."], 
+    [".",".",".",".",".",".",".","."], 
+    [".",".",".",".",".",".",".","."], 
+    ["P","P","P","P","P","P","P","P"], 
+    ["R","N","B","Q","K","B","N","R"], 
 ]
+#Displays what colour turn
+count_moves = 0
+print(check_turns(count_moves))
 
-
-
-
-#funtion is called to display
-from chess_function import DisplayBoard
+# Display the initial board state
 DisplayBoard(board)
 
-
+# Check for the availible moves
 
