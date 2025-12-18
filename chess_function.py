9693d2157a446563ad0a319264a2c198d3cee2cd
Chraeten <chraetenlategan@gmail.com>
2025-12-18 21:47:59 +0200

    Update chess_function.py

--- chess_function.py
+++ chess_function.py
@@ -1,3 +1,19 @@
+
 def DisplayBoard(board):
-    for row in board:
-        print(row)
+    for r in range(8):
+        board_order = ""
+        for c in range(8):
+            board_order += board[r][c]
+        print(board_order)
+
+
+def check_turns(number_of_moves):
+    if number_of_moves%2 == 0:
+        return("white_turn")
+    else:
+        return("black_turn")
+
+def check_available_moves(board):
+    pass
+    
+
